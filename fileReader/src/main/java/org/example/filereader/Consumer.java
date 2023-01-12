38f8152b1eed793a3d27df46618f97b5ab6931f8
tzhelyazkov <teodor.zhelyazkov99@gmail.com>
2023-01-12 16:47:12 +0200

    Consumer thread now has the following fields: order (asc or desc), sortIndex (by which index every csv file will be sorted), isInt (if this index is an integer or a string.

--- fileReader/src/main/java/org/example/filereader/Consumer.java
+++ fileReader/src/main/java/org/example/filereader/Consumer.java
@@ -6,10 +6,16 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Consumer extends Thread {
     private final AtomicInteger counter;
     private final UniqueEventsQueue<String[]> queue;
+    private final String order;
+    private final int sortIndex;
+    private final boolean isInt;
 
-    public Consumer(UniqueEventsQueue<String[]> queue, AtomicInteger counter) {
+    public Consumer(UniqueEventsQueue<String[]> queue, AtomicInteger counter, String order, int sortIndex, boolean isInt) {
         this.queue = queue;
         this.counter = counter;
+        this.order = order;
+        this.sortIndex = sortIndex;
+        this.isInt = isInt;
     }
 
     @Override
@@ -17,7 +23,7 @@ public class Consumer extends Thread {
         do
             try {
                 String[] elements = queue.poll();
-                ExternalSorter.mergesort(elements);
+                    ExternalSorter.mergesort(elements, order, sortIndex, isInt);
                 ExternalSorter.writeSortedFile(elements, counter.getAndIncrement());
             } catch (IOException | InterruptedException e) {
                 throw new RuntimeException();
