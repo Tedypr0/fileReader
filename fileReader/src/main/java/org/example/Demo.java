b56e2b7f2e373d161bd822c2c90dba38c4c607b9
tzhelyazkov <teodor.zhelyazkov99@gmail.com>
2023-01-18 10:34:11 +0200

    Main method for ExternalSorter.

---
+++ fileReader/src/main/java/org/example/Demo.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.example.filereader.ExternalSorter;
+
+import java.util.Arrays;
+
+public class Demo {
+    public static void main(String[] args) {
+        ExternalSorter externalSorter = new ExternalSorter();
+        externalSorter.start();
+    }
+
+}
