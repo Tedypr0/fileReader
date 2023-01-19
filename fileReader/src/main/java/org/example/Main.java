3642c0a394ec817ddababa089e5072b8f36676d9
tzhelyazkov <teodor.zhelyazkov99@gmail.com>
2023-01-19 11:45:49 +0200

    Demo rename

--- fileReader/src/main/java/org/example/Main.java
+++ fileReader/src/main/java/org/example/Main.java
@@ -2,9 +2,7 @@ package org.example;
 
 import org.example.filereader.ExternalSorter;
 
-import java.util.Arrays;
-
-public class Demo {
+public class Main {
     public static void main(String[] args) {
         ExternalSorter externalSorter = new ExternalSorter();
         externalSorter.start();
