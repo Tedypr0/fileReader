102418cb9e78cbafe7a36800798485e93e0bfd94
tzhelyazkov <teodor.zhelyazkov99@gmail.com>
2023-01-23 12:12:59 +0200

    Changed Consumer break logic.

--- fileReader/src/main/java/org/example/filereader/Consumer.java
+++ fileReader/src/main/java/org/example/filereader/Consumer.java
@@ -1,6 +1,7 @@
 package org.example.filereader;
 
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.example.filereader.ExternalSorter.mergesort;
@@ -8,16 +9,14 @@ import static org.example.filereader.ExternalSorter.writeSortedFile;
 
 public class Consumer extends Thread {
     private final AtomicInteger counter;
-    private final AtomicInteger neededToBeCreated;
     private final UniqueEventsQueue<String[]> queue;
     private final String order;
     private final int sortIndex;
     private final boolean isInt;
 
-    public Consumer(UniqueEventsQueue<String[]> queue, AtomicInteger counter, AtomicInteger neededToBeCreated, String order, int sortIndex, boolean isInt) {
+    public Consumer(UniqueEventsQueue<String[]> queue, AtomicInteger counter, String order, int sortIndex, boolean isInt) {
         this.queue = queue;
         this.counter = counter;
-        this.neededToBeCreated = neededToBeCreated;
         this.order = order;
         this.sortIndex = sortIndex;
         this.isInt = isInt;
@@ -25,14 +24,20 @@ public class Consumer extends Thread {
 
     @Override
     public void run() {
-        do
+        String[] elements;
+        while(true) {
             try {
-                String[] elements = queue.poll();
+                if (queue.peek()[0].equals("POISONPILL")){
+                    break;
+                }
+                elements = queue.poll();
                 mergesort(elements, order, sortIndex, isInt);
-                writeSortedFile(elements, counter.getAndIncrement());
+                writeSortedFile(elements, counter.get());
+                counter.incrementAndGet();
             } catch (IOException | InterruptedException e) {
                 throw new RuntimeException();
-            } while ((queue.size() > 0) && (counter.get() < neededToBeCreated.get()));
+            }
+        }
         System.out.println(this.getName() + " has finished its' work!");
     }
 }
