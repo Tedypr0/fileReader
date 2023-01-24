340a1f9dccb5d3ed8fe16b3407cb3537d27989a4
tzhelyazkov <teodor.zhelyazkov99@gmail.com>
2023-01-24 10:18:07 +0200

    Minor changes to Consumer class.

--- fileReader/src/main/java/org/example/filereader/Consumer.java
+++ fileReader/src/main/java/org/example/filereader/Consumer.java
@@ -13,7 +13,7 @@ public class Consumer extends Thread {
     private final String order;
     private final int sortIndex;
     private final boolean isInt;
-    private static AtomicBoolean isPoisonFound = null;
+    static AtomicBoolean isPoisonFound = null;
 
     public Consumer(UniqueEventsQueue<String[]> queue, AtomicInteger counter, String order, int sortIndex, boolean isInt, AtomicBoolean isPoisonFound) {
         this.queue = queue;
@@ -29,17 +29,15 @@ public class Consumer extends Thread {
         String[] elements;
         while (!isPoisonFound.get()) {
             try {
-                elements = ExternalSorter.peekPoll();
-                mergesort(elements, order, sortIndex, isInt);
-                writeSortedFile(elements, counter.getAndIncrement());
+                elements = ExternalSorter.peekPoll(queue);
+                if (elements != null) {
+                    mergesort(elements, order, sortIndex, isInt);
+                    writeSortedFile(elements, counter.getAndIncrement());
+                }
             } catch (IOException | InterruptedException e) {
                 throw new RuntimeException();
             }
         }
         System.out.println(this.getName() + " has finished its' work!");
-    }
-
-    public synchronized static void setIsPoisonFound(boolean val){
-        isPoisonFound.set(val);
     }
 }
